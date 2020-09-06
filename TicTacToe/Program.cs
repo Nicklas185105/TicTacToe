da6e9bbd94994a8a90047368207562c227d56161
BeydersenDev <69590230+BeydersenDev@users.noreply.github.com>
2020-09-06 17:45:25 +0200

    Console game is done, needs some cleanup. Missing wpf for playing aswell

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,36 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string playerName = "";
+            bool gameOver = false;
+
+            //Create players
+            Console.WriteLine("Enter player 1 name: ");
+            playerName = Console.ReadLine();
+            TicTacToe player1 = new TicTacToe(1, playerName);
+
+            Console.WriteLine("Enter player 2 name: (Type 'computer' if you want to play against computer)");
+            playerName = Console.ReadLine();
+            TicTacToe player2 = new TicTacToe(2, playerName);
+
+            while (!gameOver)
+            {
+                TicTacToe.intBoard();
+
+                while (!player1.PlayGame() && !player2.PlayGame())
+                    gameOver = true;
+
+                if (gameOver)
+                {
+                    Console.WriteLine("Do you want to play again? Press ESC to quit");
+                    if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                        Console.WriteLine("Good Bye!");
+                    else
+                        gameOver = false;
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
